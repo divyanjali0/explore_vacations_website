f2ac495d3554a5d76dcdbd8aadc509a66862ef4e
divyanjali0 <navoda@explorevacations.lk>
2025-12-03 11:41:09 +0530

    header updated

--- parts/footer.php
+++ parts/footer.php
@@ -2,28 +2,27 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-4 col order-lg-1 ">
-                <h6 class="footer-logo m-0"><img src="assets/images/favicon.png" alt="Ceylon Adventure Tours | Logo">
-                    Ceylon <span>Adventure</span> Tours</h6>
+                <h6 class="footer-logo m-0"><img src="assets/images/footer-logo.png" alt="Explore Vacations | Logo"></h6>
                 <p class="my-3 my-lg-4">Join us in exploring Sri Lanka, <br>where adventure meets excellence.</p>
                 <ul class="list-unstyled d-flex" id="footer-social-list">
                     <li class="me-2">
-                        <a href="https://www.facebook.com/profile.php?id=61550240346796&mibextid=LQQJ4d" target="_blank" alt="facebook">
+                        <a href="https://www.facebook.com/explorevacationssrilanka" target="_blank" alt="facebook">
                             <i class="fa-brands fa-facebook-f"></i>
                         </a>
                     </li>
                     <li class="mx-2">
-                        <a href="https://instagram.com/ceylonadventuretours?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" alt="instragram">
+                        <a href="https://www.instagram.com/explorevacationssrilanka/" target="_blank" alt="instragram">
                             <i class="fa-brands fa-instagram"></i>
                         </a>
                     </li>
                     <li class="ms-2">
-                        <a href="https://wa.me/+94777173007" target="_blank" alt="whatsapp">
+                        <a href="https://wa.me/+94773207107" target="_blank" alt="whatsapp">
                             <i class="fa-brands fa-whatsapp"></i>
                         </a>
                     </li>
                 </ul>
             </div>
-            <div class="col-6 col-md-3 col-lg-2 order-lg-2">
+            <div class="col-12 col-md-6 col-lg-4 order-lg-2">
                 <h6 class="my-4 mt-lg-0">Links</h6>
                 <ul class="list-unstyled" id="footer-nav-link-list">
                     <li>
@@ -46,58 +45,41 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-6 col-md-3 col-lg-2 order-lg-3">
-                <h6 class="my-4 mt-lg-0">Tours</h6>
-                <ul class="list-unstyled" id="footer-tour-list">
-                    <li>
-                        <a href="javascript:void(0);">Jeep Tours</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0);">Motorcycle Tours</a>
-                    </li>
-                    <li>
-                        <a href="https://srilankaroyalenfieldtours.com" target="_blank">Royal Enfield Tours</a>
-                    </li>
-                    <li>
-                        <a  href="javascript:void(0);">Tuk Tuk Tours</a>
-                    </li>
-                </ul>
-            </div>
             <div class="col-12 col-md-6 col-lg-4 order-lg-4">
                 <h6 class="my-4 mt-lg-0">Contact</h6>
                 <ul class="list-unstyled" id="footer-contact-list">
                     <li>
-                        <img src="assets/images/icons/footer-phone.svg" alt="Ceylon Adventure Tours | Footer Contact Icon" class="me-1">
-                        <a href="tel:+94777173007">+94 777 17 3007</a>
+                        <img src="assets/images/icons/footer-phone.svg" alt="Explore Vacations | Footer Contact Icon" class="me-1">
+                        <a href="tel:+94773207107">+94 773 20 7107</a>
                     </li>
                     <li>
-                        <img src="assets/images/icons/footer-email.svg" alt="Ceylon Adventure Tours | Footer Contact Icon" class="me-1">
-                        <a href="mailto:info@ceylonadventuretours.com">info@ceylonadventuretours.com</a>
+                        <img src="assets/images/icons/footer-email.svg" alt="Explore Vacations | Footer Contact Icon" class="me-1">
+                        <a href="mailto:info@explorevacations.lk">info@explorevacations.lk</a>
                     </li>
                     <li>
-                        <img src="assets/images/icons/footer-location.svg" alt="Ceylon Adventure Tours | Footer Contact Icon" class="me-1">
-                        <a href="https://maps.app.goo.gl/XsLkmG28f7L1KjRc8" target="_blank">117, Lewis Place, Negombo, Sri Lanka</a>
+                        <img src="assets/images/icons/footer-location.svg" alt="Explore Vacations | Footer Contact Icon" class="me-1">
+                        <a href="https://maps.app.goo.gl/Coi2NuS2utwZLLhC6" target="_blank">371/5 Negombo Rd, Seeduwa</a>
                     </li>
                 </ul>
             </div>
         </div>
         <div class="row">
             <div class="col-12">
-                <p class="m-0 py-4">Designed and Developed by <a href="https://pixzarloop.com" target="_blank">Pixzarloop</a>. <br class="d-md-none">© 2024</p>
+                <p class="m-0 py-4">Designed and Developed by <a href="" target="_blank">Explore Vacations</a>. <br class="d-md-none">© 2025</p>
             </div>
         </div>
     </div>
 </footer>
 
 <!--Whatsapp widget-->
-<div id="whatsapp-widget">
+<!-- <div id="whatsapp-widget">
     <div class="chat-header" id="chat-header">
         <div class="avatar-container">
             <img src="assets/images/favicon.png" alt="logo">
             <div class="online-dot"></div>
         </div>
         <div class="chat-header-info">
-            <span style="transform: translateY(2px);">Ceylon Adventure Tours</span>
+            <span style="transform: translateY(2px);">Explore Vacations</span>
             <div style="color: #eeeeee;">online</div>
 
         </div>
@@ -111,4 +93,4 @@
     <div class="chat-icon" id="chat-icon">
         <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp">
     </div>
-</div>
+</div> -->
